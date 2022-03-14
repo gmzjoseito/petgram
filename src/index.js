840ee8b285952c7717ac89951c747ca27c9ae76b
Jose Gomez <gmzjoseito@gmail.com>
2022-03-14 02:52:42 -0400

    React's installation and deploy with Vercel

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 ReactDOM.render(
-    <h1>Hola Platzi!</h1>,
-    document.getElementById('app')
+  <h1>Hola Platzi!</h1>,
+  document.getElementById('app')
 )
