67f254a90da42e23958353cdf669c3080e9542a6
Jose Gomez <gmzjoseito@gmail.com>
2022-03-18 06:16:53 -0400

    Replace db local import for fetch api deployed with Vercel, implementing React Hooks

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Category } from '../Category'
-import db from '../../../api/db.json'
 //
 import { List, Item } from './styles.js'
 
 export const ListOfCategories = () => {
+  const [categories, setCategories] = useState([])
+
+  useEffect(function () {
+    window.fetch('https://gmz-petgram-api.vercel.app/categories')
+      .then(response => response.json())
+      .then(response => {
+        setCategories(response)
+      })
+  }, [])
+
   return (
     <List>
       {
-        db.categories.map(category =>
+        categories.map(category =>
           <Item key={category.id}>
             <Category {...category} />
           </Item>
