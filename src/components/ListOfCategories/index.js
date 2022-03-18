ed047f2e00e76d904aecdb669d3ae827d9f25f26
Jose Gomez <gmzjoseito@gmail.com>
2022-03-18 19:56:05 -0400

    Creating a custom hook for ListOfCategories component

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -3,17 +3,26 @@ import { Category } from '../Category'
 //
 import { List, Item } from './styles.js'
 
-export const ListOfCategories = () => {
+function useCategoriesData () {
   const [categories, setCategories] = useState([])
-  const [showFixed, setShowFixed] = useState(false)
+  const [loading, setLoading] = useState(false)
 
   useEffect(function () {
+    setLoading(true)
     window.fetch('https://gmz-petgram-api.vercel.app/categories')
       .then(response => response.json())
       .then(response => {
         setCategories(response)
+        setLoading(false)
       })
   }, [])
+
+  return { categories, loading }
+}
+
+export const ListOfCategories = () => {
+  const { categories, loading } = useCategoriesData()
+  const [showFixed, setShowFixed] = useState(false)
 
   useEffect(function () {
     const onScroll = e => {
@@ -26,13 +35,15 @@ export const ListOfCategories = () => {
   }, [showFixed])
 
   const renderList = (fixed) => (
-    <List className={fixed ? 'fixed' : ''}>
+    <List fixed={fixed}>
       {
-        categories.map(category =>
-          <Item key={category.id}>
-            <Category {...category} />
-          </Item>
-        )
+        loading
+          ? <Item key='loading'><Category /></Item>
+          : categories.map(category =>
+            <Item key={category.id}>
+              <Category {...category} />
+            </Item>
+          )
       }
     </List>
   )
