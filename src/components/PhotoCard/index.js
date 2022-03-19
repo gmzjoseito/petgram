a92334d67540ec5ae0915b01b131edd0854254f6
Jose Gomez <gmzjoseito@gmail.com>
2022-03-18 20:34:02 -0400

    Lazy loading added to PhotoCard component with Intersection Observer

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,22 +1,40 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 //
 import { MdFavoriteBorder } from 'react-icons/md'
-import { ImgWrapper, Image, Button } from './styles.js'
+import { Article, ImgWrapper, Image, Button } from './styles.js'
 
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
+  const elementRef = useRef(null)
+  const [show, setShow] = useState(false)
+
+  useEffect(function () {
+    const observer = new window.IntersectionObserver(function (entries) {
+      const { isIntersecting } = entries[0]
+      if (isIntersecting) {
+        setShow(true)
+        observer.disconnect()
+      }
+    })
+    observer.observe(elementRef.current)
+  }, [elementRef])
+
   return (
-    <article>
-      <a href={`/detail/${id}`}>
-        <ImgWrapper>
-          <Image src={src} />
-        </ImgWrapper>
-      </a>
-      <Button>
-        <MdFavoriteBorder size='32px' />
-        {likes} likes!
-      </Button>
-    </article>
+    <Article ref={elementRef}>
+      {
+        show && <>
+          <a href={`/detail/${id}`}>
+            <ImgWrapper>
+              <Image src={src} />
+            </ImgWrapper>
+          </a>
+          <Button>
+            <MdFavoriteBorder size='32px' />
+            {likes} likes!
+          </Button>
+        </> //eslint-disable-line
+      }
+    </Article>
   )
 }
