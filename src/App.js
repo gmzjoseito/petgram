f79b54161168be1d0e5395d0462f7094a9d81fee
Jose Gomez <gmzjoseito@gmail.com>
2022-03-25 05:01:00 -0400

    Private 'favs' and 'user' routes added

--- src/App.js
+++ src/App.js
@@ -4,9 +4,16 @@ import { Logo } from './components/Logo'
 //
 import { Home } from './pages/Home'
 import { Detail } from './pages/Detail'
+import { Favs } from './pages/Favs'
+import { User } from './pages/User'
+import { NotRegisteredUser } from './pages/NotRegisteredUser'
 import { Navbar } from './components/Navbar'
 //
 import { Router } from '@reach/router'
+
+const UserLogged = ({ children }) => {
+  return children({ isAuth: false })
+}
 
 export const App = () => {
   return (
@@ -18,6 +25,20 @@ export const App = () => {
         <Home path='/pet/:id' />
         <Detail path='/detail/:detailId' />
       </Router>
+      <UserLogged>
+        {
+          ({ isAuth }) =>
+            isAuth
+              ? <Router>
+                <Favs path='/favs' />
+                <User path='/user' />
+              </Router> //eslint-disable-line
+              : <Router>
+                <NotRegisteredUser path='/favs' />
+                <NotRegisteredUser path='/user' />
+              </Router> //eslint-disable-line
+        }
+      </UserLogged>
       <Navbar />
     </>
   )
